39e4f955e0877b32ad1b9602789fecc36b08e025
David Pick <pickdavid@gmail.com>
2010-11-20 19:58:35 -0600

    naive implementation of is_prime?

--- prime_factors.rb
+++ prime_factors.rb
@@ -3,7 +3,37 @@ def prime_factors(n)
   return [n]
 end
 
+def is_prime?(n)
+  2.upto(n - 1).each do |i|
+    return false if n % i == 0  
+  end
+
+  return true
+end
+
 ########## Tests ############
+
+describe "is_prime? tests" do
+  it "should return true for 2" do
+    is_prime?(2).should be_true
+  end
+
+  it "should return true for 3" do
+    is_prime?(3).should be_true
+  end
+
+  it "should return false for 4" do
+    is_prime?(4).should be_false
+  end
+
+  it "should return true for 5" do
+    is_prime?(5).should be_true
+  end
+
+  it "should return false for 6" do
+    is_prime?(6).should be_false
+  end
+end
 
 describe "prime_factors tests" do
   it "should return 2 for 2" do
