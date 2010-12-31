cc45ed2c8c23f80e5cdf87bdd1559b800c45da36
David Pick <pickdavid@gmail.com>
2010-12-31 15:32:14 -0600

    yay even shorter :P

--- Sieve/sieve.rb
+++ Sieve/sieve.rb
@@ -1,11 +1,9 @@
 class Sieve
   def primes_below(n)
-    nums = 2.upto(n).to_a
+    nums = (2..n).to_a
 
     nums.each do |num|
       nums.map! { |i| i % num == 0 && i != num ? nil : i }.compact!
     end
-
-    return nums
   end
 end
